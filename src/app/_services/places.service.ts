4a5b636fd2d87ed47536c3549d2459500ed7a389
zdnuruDev <doronzur780@gmail.com>
2017-08-16 19:18:22 +0300

    new feed and place details

--- src/app/_services/places.service.ts
+++ src/app/_services/places.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/from';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 
 export interface IPlace {
   id: string;
@@ -12,13 +13,15 @@ export interface IPlace {
 
 @Injectable()
 export class PlacesService {
-  private placesApi = 'http://localhost:8000/Places';
+  private placesApi = 'http://10.0.0.2:8080/places';
+  private placesByLocation = "/getPlacesByLatLng/";
+  private placeById = "/getPlaceById/";
   private places: JSON[];
 
   constructor (private http: Http) {}
 
   public getPlaces(): Observable<JSON[]> {
-    return this.http.get(this.placesApi)
+    return this.http.get(this.placesApi+this.placesByLocation+"32.062918/34.7716473")
                     .map(this.extractData);
   }
 
@@ -26,13 +29,11 @@ export class PlacesService {
     this.places = places;
   }
 
-  public getPlaceById(id: number) {
-   /* return this.http.get(this.placesApi + '/' + id)
-      .map(this.extractData);*/
-   if (!this.places) {
-     return;
-   }
-   return this.places.find((place: any) => place.id === id.toString());
+  public getPlaceById(id: String) {
+    return this.http.get(this.placesApi + this.placeById + id)
+      .map(this.extractData);
+   //   var source = Observable.from(this.places).filter((x: any)=>  x.id === id.toString());
+   //   return source;
   }
 
   private extractData(res: Response) {
